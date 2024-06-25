6cf7f021794ba3ab47aa58fe8630d132ae5c4037
Igor Ivantsiv <ig.ivantsiv@hotmail.co.uk>
2024-06-25 17:16:58 +0200

    initial commit

---
+++ you-are-not-an-idiot-sandwich/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*
+const recipes = [
+  {dishName: "lasagne", 
+    ingredients: [
+    haveInPantry: [""],
+    vegetables: [""],
+    meat: ["", ""],
+    sides: ["",""]
+  ]
+  duration: 3,
+  servings: 2,
+  instructions: []
+} 
+]
+*/
+
+function App() {
+
+  return (
+    <>
+    <navbar>
+      <img src="" alt="logo" />
+      <div>company name</div>
+      <img src="" alt="menu icon" />
+      <div>
+        <a href="">Home</a>
+        <a href="">About Us</a>
+      </div>
+    </navbar>
+    <section className='content'>
+
+    </section>
+    <footer>
+
+    </footer>
+    </>
+  )
+}
+
+export default App
