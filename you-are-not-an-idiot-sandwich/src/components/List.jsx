4e37a4b93960954e11106108115187167434b940
Igor Ivantsiv <ig.ivantsiv@hotmail.co.uk>
2024-06-26 19:09:14 +0200

    adding listItem component

--- you-are-not-an-idiot-sandwich/src/components/List.jsx
+++ you-are-not-an-idiot-sandwich/src/components/List.jsx
@@ -1,20 +1,14 @@
 import recipesData from "../assets/recipes.json";
 import React, { useState } from "react";
+import ListItem from "./ListItem.jsx";
 
 function List() {
-  const [functionData, setFunctionData] = useState(recipesData);
-  return functionData.map((eachRecipe) => {
-    return <> 
-    <div className="recipeCard" key ={eachRecipe.id} id = {eachRecipe.id}>
-        <img src={eachRecipe.image} alt={`${eachRecipe.name} image`} />
-        <h3>{eachRecipe.name}</h3>
-        <p>{eachRecipe.calories}</p>
-        <p>{eachRecipe.servings}</p>
-        <button type="button" className="detailButton">Details</button>
-        <button type="button" className="deleteButton">Delete</button>
-    </div>
-</>;
-  });
+    return  recipesData.map((eachRecipe)=>{
+      return<>
+      <ListItem key ={eachRecipe.id} recipe={ eachRecipe } />
+      </>
+    })
+
 }
 
-export default List;
+export default List; 
---
+++ you-are-not-an-idiot-sandwich/src/components/ListItem.jsx
@@ -0,0 +1,16 @@
+function ListItem({ recipe }) {
+    return <> 
+    <div className="recipeCard" key ={recipe.id} id = {recipe.id}>
+        <img className="recipeImage" src={recipe.image} alt={`${recipe.name} image`} />
+        <div className="cardContent">
+        <h2>{recipe.name}</h2>
+        <p><strong>Calories: </strong>{recipe.calories}</p>
+        <p><strong>Servings: </strong>{recipe.servings}</p>
+        <button type="button" className="detailButton">Details</button>
+        <button type="button" className="deleteButton">Delete</button>
+        </div>
+    </div>
+</>;
+}
+
+export default ListItem
