ae0f0054751631972d4ef5d97812cedae15de1e9
Igor Ivantsiv <ig.ivantsiv@hotmail.co.uk>
2024-06-27 17:37:52 +0200

    wip

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,27 @@
 import recipesData from "../assets/recipes.json";
 import ListItem from "./ListItem.jsx";
+import { useState } from "react";
 
 function List() {
+  const [functionData, setFunctionData] = useState(recipesData);
 
+  const handleDelete = (recipeId) => {
+    console.log(recipeId);
+    const filteredData = functionData.filter(
+      (currentRecipe) => currentRecipe.id !== recipeId
+    );
+    console.log(filteredData);
+    setFunctionData(filteredData);
+  };
+
+  return functionData.map((eachRecipe) => {
     return (
-
-      recipesData.map((eachRecipe)=>{
-      return <ListItem key ={eachRecipe.id} recipe={ eachRecipe } />
-    })
-
-)}
-export default List; 
+      <ListItem
+        key={eachRecipe.id}
+        recipe={eachRecipe}
+        handleDelete={handleDelete}
+      />
+    );
+  });
+}
+export default List;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,27 +1,36 @@
-import recipesData from "../assets/recipes.json";
-import { useState } from "react";
-
-function ListItem({ recipe }) {
-
-
-    const [recipes, setRecipes] = useState(recipesData)
-
-    const handleDelete = (id) => {
-      setRecipes(recipes.filter(currentRecipe => currentRecipe.id === id))
-    }
-
-    return <> 
-    <div className="recipeCard" key ={recipe.id} id = {recipe.id}>
-        <img className="recipeImage" src={recipe.image} alt={`${recipe.name} image`} />
+function ListItem({ recipe, handleDelete }) {
+  return (
+    <>
+      <div className="recipeCard" key={recipe.id} id={recipe.id}>
+        <img
+          className="recipeImage"
+          src={recipe.image}
+          alt={`${recipe.name} image`}
+        />
         <div className="cardContent">
-        <h2>{recipe.name}</h2>
-        <p><strong>Calories: </strong>{recipe.calories}</p>
-        <p><strong>Servings: </strong>{recipe.servings}</p>
-        <button type="button" className="detailButton">Details</button>
-        <button type="button" className="deleteButton" onClick={()=>handleDelete(recipe.id)}>Delete</button>
+          <h2>{recipe.name}</h2>
+          <p>
+            <strong>Calories: </strong>
+            {recipe.calories}
+          </p>
+          <p>
+            <strong>Servings: </strong>
+            {recipe.servings}
+          </p>
+          <button type="button" className="detailButton">
+            Details
+          </button>
+          <button
+            type="button"
+            className="deleteButton"
+            onClick={() => handleDelete(recipe.id)}
+          >
+            Delete
+          </button>
         </div>
-    </div>
-</>;
+      </div>
+    </>
+  );
 }
 
-export default ListItem
+export default ListItem;
